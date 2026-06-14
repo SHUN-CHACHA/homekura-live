15306b69abf3dcf0c26601d03f65f67da908c116
SHUN-CHACHA <eisyun@gmail.com>
2026-06-14 18:29:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,249 +1,305 @@
-// config.js
-// APIキーと初期配信者リストをまとめて管理します
-
 const CONFIG = {
-    YOUTUBE_API_KEY: 'AIzaSyDoy4uvy_4jXcpGw8fxTE33lFmBn18fAYY',
-
-    // 配信者一覧データ
+    YOUTUBE_API_KEY: "AIzaSyDoy4uvy_4jXcpGw8fxTE33lFmBn18fAYY",
     DEFAULT_CHANNELS: [
         {
                 "name": "まぐにぃ",
                 "id": "UCMP7QuS4suoONg47Nbi-wrg",
+                "minecraftId": "maguro29",
                 "twitterId": "maguro29",
                 "twitchId": "maguro29game",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "https://discord.gg/bRuT74X7sS",
-                "color": "#ff9900"
+                "color": "#2bff00",
+                "avatarImg": "avatars/maguro29.png"
         },
         {
                 "name": "じゃじゃーん菊地",
                 "id": "UCi2kiw8hMo0vMAh5lXiafug",
+                "minecraftId": "jajaankikuchi",
                 "twitterId": "kikuchidaisuke",
                 "twitchId": "",
-                "tiktokId": "jajakiku",
+                "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ffff00"
+                "color": "#ffff00",
+                "avatarImg": "avatars/jajaankikuchi.png"
         },
         {
                 "name": "さかいさんだー",
                 "id": "UCuk8ABJTVWiApYlEojW_QrA",
+                "minecraftId": "Sakai_Thunder",
                 "twitterId": "Sakai_Thunder",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ff00ff"
+                "color": "#ff00ff",
+                "avatarImg": "avatars/Sakai_Thunder.png"
         },
         {
                 "name": "Ｍさん",
                 "id": "UCGK0iFuXhfA5VnL64xiKEmQ",
+                "minecraftId": "MrMsan",
                 "twitterId": "maikura_souti",
                 "twitchId": "msan_no_nitizyou",
-                "tiktokId": "msan_official",
+                "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#00ffff"
+                "color": "#00ffff",
+                "avatarImg": "avatars/MrMsan.png"
         },
         {
                 "name": "タツナミ先生",
                 "id": "UCedenULTLYf3DanflEXdRCQ",
+                "minecraftId": "syuutan",
                 "twitterId": "tatsunami",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#00ff00"
+                "color": "#00ff00",
+                "avatarImg": "avatars/syuutan.png"
         },
         {
                 "name": "神近",
                 "id": "UCdLe2q91fJkN9GEyDvItp-w",
+                "minecraftId": "kamichika",
                 "twitterId": "t_kamichika",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ffffff"
+                "color": "#f59324",
+                "avatarImg": "avatars/kamichika.png"
         },
         {
                 "name": "しろくる",
                 "id": "UCTl5XpFddyDsj5woStUPu0w",
+                "minecraftId": "sirokuru",
                 "twitterId": "sirokuru_",
                 "twitchId": "sirokuru",
-                "tiktokId": "sirokuru",
+                "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ffffff"
+                "color": "#ffffff",
+                "avatarImg": "avatars/sirokuru.png"
         },
         {
                 "name": "たにし",
                 "id": "UCiC8VCiSLD3mxoO-QFKQnBQ",
+                "minecraftId": "Tanish1",
                 "twitterId": "tanishi_manbo",
                 "twitchId": "tanishi1200",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#0000ff"
+                "color": "#00ff7b",
+                "avatarImg": "avatars/Tanish1.png"
         },
         {
                 "name": "凸もり",
                 "id": "UCdN9GjPrqhcrzE27eWNNjxg",
+                "minecraftId": "totsumori",
                 "twitterId": "mk_2_totsu",
                 "twitchId": "totsumori",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#888888"
+                "color": "#888888",
+                "avatarImg": "avatars/totsumori.png"
         },
         {
                 "name": "神宮寺ちゃんねる",
                 "id": "UCou8IVB8jJPk56H0farYQdA",
+                "minecraftId": "jingujich",
                 "twitterId": "jinguji777ch",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ff0000"
+                "color": "#00bfff",
+                "avatarImg": "avatars/jingujich.png"
         },
         {
                 "name": "カズクラ",
                 "id": "UCu3Mp1ZimtNvyA-bcfo9VrQ",
+                "minecraftId": "kazuchannel",
                 "twitterId": "kazuch0924",
                 "twitchId": "",
-                "tiktokId": "super_kazukura",
+                "tiktokId": "",
                 "instaId": "kazuch0924",
                 "discordUrl": "",
-                "color": "#ff0000"
+                "color": "#ff0000",
+                "avatarImg": "avatars/kazuchannel.png"
         },
         {
                 "name": "たいたい",
                 "id": "UCds-nvoKCcvixVXJ_54QcbQ",
+                "minecraftId": "taitai0904",
                 "twitterId": "taitaiMEN1997",
                 "twitchId": "taitai19970904",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#00ccff"
+                "color": "#ffdd00",
+                "avatarImg": "avatars/taitai0904.png"
         },
         {
                 "name": "岐阜のこみちん",
                 "id": "UCw4frqkotySLjJ_CFZR5-7w",
+                "minecraftId": "komichin",
                 "twitterId": "komichin0704",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ffcc00"
+                "color": "#ffcc00",
+                "avatarImg": "avatars/komichin.png"
         },
         {
                 "name": "ナナホシナナ",
                 "id": "UCncsOsnMBAAaYb6WidBYIDw",
+                "minecraftId": "nanahoshiNanaVT",
                 "twitterId": "nanahoshinana77",
                 "twitchId": "nanahoshinana77",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ff33ff"
+                "color": "#ff33ff",
+                "avatarImg": "avatars/nanahoshiNanaVT.png"
         },
         {
                 "name": "月成るくす",
                 "id": "UCP9rcDSyTRtnu8EsG2CmLMg",
+                "minecraftId": "tsukinarilux",
                 "twitterId": "Tsukinari_Lux_",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "tsukinarilux",
                 "discordUrl": "",
-                "color": "#ffffcc"
+                "color": "#bcec55",
+                "avatarImg": "avatars/tsukinarilux.png"
         },
         {
                 "name": "メッス",
                 "id": "UCkJeBcOPRBs5bri5B3sFgVg",
+                "minecraftId": "messu009",
                 "twitterId": "messu_009",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ffffff"
+                "color": "#ffffff",
+                "avatarImg": "avatars/messu009.png"
         },
         {
                 "name": "よいよい",
                 "id": "UCzk-vAIbo3iVBGTNaJQS2qQ",
+                "minecraftId": "yoiy0i",
                 "twitterId": "yoiy0i",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ccff00"
+                "color": "#ff0000",
+                "avatarImg": "avatars/yoiy0i.png"
         },
         {
                 "name": "うえまさ",
                 "id": "UCIBDdDXnViOgN5tCpfX7NOA",
+                "minecraftId": "uema5a",
                 "twitterId": "uema5aCh",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "uema5a",
                 "discordUrl": "",
-                "color": "#9900ff"
+                "color": "#0008ff",
+                "avatarImg": "avatars/uema5a.png"
         },
         {
                 "name": "久那式リン",
                 "id": "UCPs7s2VlkwVWWpjcyjviINA",
+                "minecraftId": "Kunashiki_Lin",
                 "twitterId": "Kunashiki_Lin_",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ffffff"
+                "color": "#afebf3",
+                "avatarImg": "avatars/Kunashiki_Lin.png"
         },
         {
                 "name": "犬野はる",
                 "id": "UCVB16vOi24WElLNbbmOFFsA",
+                "minecraftId": "INUNOHARU",
                 "twitterId": "VT_InunoHaru",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ccffcc"
+                "color": "#ccffcc",
+                "avatarImg": "avatars/INUNOHARU.png"
         },
         {
                 "name": "珍珠花こまり",
                 "id": "UCSLCZ5uhCYUMQ9TG9Cs1ERQ",
+                "minecraftId": "yukiyanagiKOMARI",
                 "twitterId": "komari_yuki",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#ffffff"
+                "color": "#ffe5e5",
+                "avatarImg": "avatars/yukiyanagiKOMARI.png"
         },
         {
                 "name": "ぽこにゃん",
                 "id": "UCO06KZjWOe6b1tXrgzzakZA",
-                "twitterId": "",
+                "minecraftId": "pokonyan_0603",
+                "twitterId": "pokonyan1185",
+                "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#00ff00"
+                "color": "#00ff00",
+                "avatarImg": "avatars/pokonyan_0603.png"
         },
         {
                 "name": "KtR（こーたろー）",
                 "id": "UCuJVrV6spfu9QUP6Wr4fugA",
+                "minecraftId": "HAOgame_KtR",
                 "twitterId": "HAOKtR",
                 "twitchId": "",
                 "tiktokId": "",
                 "instaId": "",
                 "discordUrl": "",
-                "color": "#3399ff"
+                "color": "#ff3333",
+                "avatarImg": "avatars/HAOgame_KtR.png"
+        },
+        {
+                "name": "桜咲千依",
+                "id": "UCynBu4jMSaOLQ8OSQMowP1g",
+                "minecraftId": "chiyohamstar",
+                "twitterId": "chiyosab22",
+                "twitchId": "",
+                "tiktokId": "",
+                "instaId": "",
+                "discordUrl": "",
+                "color": "#ffffff",
+                "avatarImg": "avatars/chiyohamstar.png"
         },
         {
                 "name": "ゆりも",
                 "id": "UCrfGCEk3MPYap72t0TX_sdQ",
-                "twitterId": "yurimotosuzu",
+                "minecraftId": "yurimosaaan",
+                "twitterId": "@yurimotosuzu",
                 "twitchId": "yurimotosuzu",
-                "tiktokId": "yurimotosuzu",
-                "instaId": "yurimotosuzu",
+                "tiktokId": "",
+                "instaId": "",
                 "discordUrl": "",
-                "color": "#a6d3c4"
+                "color": "#aae4d3",
+                "avatarImg": "avatars/yurimosaaan.png"
         }
 ]
 };
