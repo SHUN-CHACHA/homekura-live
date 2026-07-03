da07a7b63e8e5b90768c1b5fd5e444948a8013e7
SHUN-CHACHA <eisyun@gmail.com>
2026-07-03 18:47:42 +0900

    Restore member data structure and comments

--- memberlist.js
+++ memberlist.js
@@ -15,7 +15,7 @@ const memberData = [
     { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 04人目
     { area: "エリア05", name: "うどん", icon: "UDON130.svg", name2: "", icon2: "", x: -360, z: 624 }, // 05人目
     { area: "エリア06", name: "黎乃鈴 -Kurono Suzu-", icon: "kuronosuzu.svg", name2: "", icon2: "", x: -624, z: 360 }, // 06人目
-    { area: "エリア07", name: "神宮寺ナナホシナナ", icon: "jingujich.svg", name2: "", icon2: "", x: -720, z: 0 }, // 07人目
+    { area: "エリア07", name: "神宮寺", icon: "jingujich.svg", name2: "", icon2: "", x: -720, z: 0 }, // 07人目
     { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 08人目
     { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 09人目
     { area: "エリア10", name: "たにし", icon: "Tanish1.svg", name2: "珍珠花こまり", icon2: "yukiyanagiKOMARI.svg", x: 0, z: -720 }, // 10人目
