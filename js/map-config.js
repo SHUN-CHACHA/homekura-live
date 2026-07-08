4dfe210efff85e2be6ccb6e37dc346fe76c30fe6
SHUN-CHACHA <eisyun@gmail.com>
2026-07-08 10:49:34 +0900

    Update map image file name to '7月4日.png'

--- js/map-config.js
+++ js/map-config.js
@@ -13,7 +13,7 @@
 
     const mapImageList = [
         {
-            fileName: 'map/0704world_map.png',
+            fileName: 'map/7月4日.png',
             // [X座標, Z座標] の順で、画像の対角の2点を指定します（順不同でOK）。
             // 例：Chunkbaseで切り抜いた範囲が「X: -1600〜1600、Z: -1600〜1600」の場合
             //     bounds: [[-1600, -1600], [1600, 1600]]
