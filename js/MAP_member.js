93687f99b0a987f96c2ec5a8c571d3e2c90aa1b0
SHUN-CHACHA <eisyun@gmail.com>
2026-07-15 08:13:32 +0900

    Enhance member data structure for additional icons
    Added support for a third member icon and name in the member data structure.

--- js/MAP_member.js
+++ js/MAP_member.js
@@ -6,6 +6,8 @@
 //         空欄のままだとプレースホルダー画像が表示されます
 // name2 / icon2 : 同じ拠点に2人目がいる場合はここに記入すると
 //                 アイコンが2つ並んで表示されます
+// name3 / icon3 : 同じ拠点に3人目がいる場合はここに記入すると
+//                 アイコンが3つ並んで表示されます（1人1行で改行表示）
 // area / name が空欄の行は「未登録」として地図にも一覧にも表示されません
 // ==========================================================
 const memberData = [
@@ -20,7 +22,7 @@ const memberData = [
     { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 09人目
     { area: "エリア10", name: "たにし", icon: "Tanish1.svg", name2: "珍珠花こまり", icon2: "yukiyanagiKOMARI.svg", x: 0, z: -720 }, // 10人目
     { area: "エリア11", name: "月成るくす", icon: "tsukinarilux.svg", name2: "", icon2: "", x: 360, z: -624 }, // 11人目
-    { area: "エリア12", name: "メッス", icon: "messu009.svg", name2: "", icon2: "", x: 624, z: -360 }, // 12人目
+    { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 12人目
     { area: "エリア13", name: "ぽこにゃん", icon: "pokonyan_0603.svg", name2: "", icon2: "", x: 966, z: 259 }, // 13人目
     { area: "エリア14", name: "久那式リン", icon: "Kunashiki_Lin.svg", name2: "", icon2: "", x: 707, z: 707 }, // 14人目
     { area: "共有地", name: "共有地", icon: "HOME.png", name2: "", icon2: "", x: 514, z: 817 }, // 共有地
@@ -28,7 +30,7 @@ const memberData = [
     { area: "エリア16", name: "神近", icon: "kamichika.svg", name2: "", icon2: "", x: -259, z: 966 }, // 16人目
     { area: "エリア17", name: "早乙女燈真", icon: "saotomeirix.svg", name2: "久遠星那", icon2: "kuon_irix.svg", x: -707, z: 707 }, // 17人目
     { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 18人目
-    { area: "エリア19", name: "瑞木ゆき", icon: "MizukiYuki22.svg", name2: "", icon2: "", x: -966, z: -259 }, // 19人目
+    { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 19人目
     { area: "エリア20", name: "じゃじゃーん菊池", icon: "jajaankikuchi.svg", name2: "", icon2: "", x: -707, z: -707 }, // 20人目
     { area: "エリア21", name: "さかいさんだー", icon: "Sakai_Thunder.svg", name2: "", icon2: "", x: -259, z: -966 }, // 21人目
     { area: "", name: "", icon: "", name2: "", icon2: "", x: 0, z: 0 }, // 22人目
